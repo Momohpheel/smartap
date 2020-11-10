570045978d12003985f313a78e78d0cf542f4f95
Momoh Philip <momohmayowa14@gmail.com>
2020-11-10 12:45:01 +0100

    fixes

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -110,7 +110,7 @@ class ClientController extends Controller
             $company = Client::where('id', auth()->user()->id)->first();
             $users = User::where('company_token', $company->token)->get();
 
-            if($users != []){
+            if($users == []){
 
                     foreach($users as $user){
                         $data[] = [
